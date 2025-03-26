cb88b3bbc8dfcd0df862067e3cf5814340b626d0
JakeKwak95 <wlghrhkr@gmail.com>
2025-03-26 18:12:29 +0900

    7강 완료

---
+++ Assets/01_Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Enemy[] enemyPrefabs;
+    public Player player;
+
+    public float spawnTime = 1f;
+    float timer;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > spawnTime)
+        {
+            SpawnEnemy();
+            timer = 0;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        int randomIndex = Random.Range(0, enemyPrefabs.Length);
+        Enemy enemy = Instantiate(enemyPrefabs[randomIndex], transform.position, transform.rotation);
+        enemy.player = player;
+    }
+}
